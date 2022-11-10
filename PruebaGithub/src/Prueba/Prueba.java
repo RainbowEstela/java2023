01e0328befa5f12f3f6e1a2c324af57553028106
usuario1daw <eloiseruiz15@gmail.com>
2022-11-10 20:02:02 +0100

    mi primer archivo

--- PruebaGithub/src/Prueba/Prueba.java
+++ PruebaGithub/src/Prueba/Prueba.java
@@ -4,7 +4,7 @@ public class Prueba {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Este fichero va a aparecer en Github");
+		System.out.println("Este fichero va a a parecer en Github");
 		
 	}
 

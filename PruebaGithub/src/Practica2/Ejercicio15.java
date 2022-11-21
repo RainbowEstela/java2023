3d2e2f8ab9b70d62287767f88ccae5e250fcdefc
usuario1daw <eloiseruiz15@gmail.com>
2022-11-21 20:28:59 +0100

    update 21/11/2022 20:28 (ejercicios hechos)

--- PruebaGithub/src/Practica2/Ejercicio15.java
+++ PruebaGithub/src/Practica2/Ejercicio15.java
@@ -3,7 +3,17 @@ package Practica2;
 public class Ejercicio15 {
 
 	public static void main(String[] args) {
+		double miDinero = 2000, interes = 0.0275, hacienda = 0.18;
+		double generado;
 		
+		//interes en un año
+		generado = miDinero * interes;
+		generado = generado - (generado * hacienda);
+		
+		//interes en medio año
+		generado = generado / 2;
+		
+		System.out.println("me corresponden " + generado + " euros");
 
 	}
 

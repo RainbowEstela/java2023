160c8d3adac98f6e5e0f435ad7e1a9dbaefc4a73
user <eloiseruiz15@gmail.com>
2022-12-02 11:02:20 +0100

    2/12/22 11:02

--- PruebaGithub/src/Practica3/Ejercicio27.java
+++ PruebaGithub/src/Practica3/Ejercicio27.java
@@ -1,10 +1,53 @@
 package Practica3;
 
+import java.util.Scanner;
+
 public class Ejercicio27 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int numero = 0, intentos = 0, mayor, menor;
+		String estado = "";
+		boolean acierto = false;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		//decir un numero entre 1 y 100
+		System.out.println("piensa un número entre 1 y 100");
+		
+		mayor = 100;
+		menor = 1;
+		while (acierto == false) {
+			numero = (int) (Math.random() *  ( mayor + 1 - menor)) + menor;
+			
+			System.out.print("¿tu número es mayor, menor o igual a " + numero + "?");
+			try {
+				estado = sc.nextLine();
+			} catch(Exception e) {
+				System.out.println("Error: caracter invalido");
+				continue;
+			}
+			
+			switch (estado) {
+			case "mayor": {
+				menor = numero + 1;
+				break;
+			}
+			case "menor": {
+				mayor = numero - 1;
+				break;
+			}
+			case "igual": {
+				acierto = true;
+				break;
+			}
+			default:
+				System.out.println("Error: instruccion no válida");
+				continue;
+			}
+			
+			intentos++;
+		}
+		System.out.println("Necesité " + intentos + " intentos para acertar tu número");
 	}
 
 }

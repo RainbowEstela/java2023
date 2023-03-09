569457fdb38f75f9d804767698ce07c1ac9ad0ec
user <eloiseruiz15@gmail.com>
2023-03-09 09:47:50 +0100

    ejercicio 4 casi hecho

--- Practica10/src/Ejercicio4/FechaUtils.java
+++ Practica10/src/Ejercicio4/FechaUtils.java
@@ -23,9 +23,76 @@ public class FechaUtils {
 		
 	}
 	
-	public static String diaCumple(LocalDateTime fechaCumple, int year) {
+	public static String diaCumple(LocalDate fechaCumple, int year) {
 		return fechaCumple.withYear(year).getDayOfWeek().toString();
 	}
 	
+	public static int numYears(LocalDate fechaCumple) {
+		return Period.between(fechaCumple, LocalDate.now()).getYears();
+	}
 	
+	public static String zodiaco(LocalDate fecha) {
+			
+		// Aries
+		if (( fecha.getDayOfMonth() >= 21 && fecha.getDayOfMonth() <= 31 && fecha.getMonthValue() == 3) || ( fecha.getDayOfMonth() >= 1 && fecha.getDayOfMonth() <= 19 && fecha.getMonthValue() == 4)) {
+			return "Aries";
+		}
+		
+		//Tauro
+		if (( fecha.getDayOfMonth() >= 20 && fecha.getDayOfMonth() <= 30 && fecha.getMonthValue() == 4) || ( fecha.getDayOfMonth() >= 1 && fecha.getDayOfMonth() <= 20 && fecha.getMonthValue() == 5)) {
+			return "Tauro";
+		}
+		
+		//geminis
+		if (( fecha.getDayOfMonth() >= 21 && fecha.getDayOfMonth() <= 31 && fecha.getMonthValue() == 5) || ( fecha.getDayOfMonth() >= 1 && fecha.getDayOfMonth() <= 20 && fecha.getMonthValue() == 6)) {
+			return "Geminis";
+		}
+		
+		//cancer
+		if (( fecha.getDayOfMonth() >= 21 && fecha.getDayOfMonth() <= 30 && fecha.getMonthValue() == 6) || ( fecha.getDayOfMonth() >= 1 && fecha.getDayOfMonth() <= 22 && fecha.getMonthValue() == 7)) {
+			return "Cancer";
+		}
+		
+		//leo
+		if (( fecha.getDayOfMonth() >= 23 && fecha.getDayOfMonth() <= 31 && fecha.getMonthValue() == 7) || ( fecha.getDayOfMonth() >= 1 && fecha.getDayOfMonth() <= 22 && fecha.getMonthValue() == 8)) {
+			return "Leo";
+		}
+		
+		//virgo
+		if (( fecha.getDayOfMonth() >= 23 && fecha.getDayOfMonth() <= 31 && fecha.getMonthValue() == 8) || ( fecha.getDayOfMonth() >= 1 && fecha.getDayOfMonth() <= 22 && fecha.getMonthValue() == 9)) {
+			return "Virgo";
+		}
+		
+		//libra
+		if (( fecha.getDayOfMonth() >= 23 && fecha.getDayOfMonth() <= 30 && fecha.getMonthValue() == 9) || ( fecha.getDayOfMonth() >= 1 && fecha.getDayOfMonth() <= 22 && fecha.getMonthValue() == 10)) {
+			return "Libra";
+		}
+		
+		//escorpio
+		if (( fecha.getDayOfMonth() >= 23 && fecha.getDayOfMonth() <= 31 && fecha.getMonthValue() == 10) || ( fecha.getDayOfMonth() >= 1 && fecha.getDayOfMonth() <= 21 && fecha.getMonthValue() == 11)) {
+			return "Escorpio";
+		}
+		
+		//Sagitario
+		if (( fecha.getDayOfMonth() >= 22 && fecha.getDayOfMonth() <= 30 && fecha.getMonthValue() == 11) || ( fecha.getDayOfMonth() >= 1 && fecha.getDayOfMonth() <= 21 && fecha.getMonthValue() == 12)) {
+			return "Sagitario";
+		}
+		
+		//Capricornio
+		if (( fecha.getDayOfMonth() >= 22 && fecha.getDayOfMonth() <= 31 && fecha.getMonthValue() == 12) || ( fecha.getDayOfMonth() >= 1 && fecha.getDayOfMonth() <= 19 && fecha.getMonthValue() == 1)) {
+			return "Capricornio";
+		}
+		
+		//acuario
+		if (( fecha.getDayOfMonth() >= 20 && fecha.getDayOfMonth() <= 31 && fecha.getMonthValue() == 1) || ( fecha.getDayOfMonth() >= 1 && fecha.getDayOfMonth() <= 18 && fecha.getMonthValue() == 2)) {
+			return "Acuario";
+		}
+		
+		//piscis
+		if (( fecha.getDayOfMonth() >= 19 && fecha.getDayOfMonth() <= 29 && fecha.getMonthValue() == 2) || ( fecha.getDayOfMonth() >= 1 && fecha.getDayOfMonth() <= 20 && fecha.getMonthValue() == 3)) {
+			return "Piscis";
+		}
+		
+		return "";
+	}
 }
--- Practica10/src/Ejercicio4/Test.java
+++ Practica10/src/Ejercicio4/Test.java
@@ -1,6 +1,7 @@
 package Ejercicio4;
 
 import java.time.Duration;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
@@ -8,10 +9,13 @@ import java.time.Month;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println(FechaUtils.diaCumple(LocalDateTime.of(2023, Month.DECEMBER, 9, 19, 30, 20),2026) );
-		System.out.println(Duration.between(LocalTime.of(12, 12, 12), LocalTime.of(0, 0, 1)));
+		System.out.println(FechaUtils.diaCumple(LocalDate.of(2023, Month.DECEMBER, 9),2026) );
+		
 		FechaUtils.cuentaAtras(LocalDateTime.of(2023, Month.DECEMBER, 9, 19, 30, 20), "felicidad");
-
+		
+		System.out.println(FechaUtils.numYears(LocalDate.of(1998, 6, 22)));
+		
+		System.out.println(FechaUtils.zodiaco(LocalDate.of(1998, 8, 12)));
 	}
 
 }
